cd5b2196b0e79d5222bd7649590118e34c2b963d
Josh Kalsi <josh.kalsi@gmail.com>
2024-07-29 23:06:23 +0100

    Run Prettier linting on all files and exclude "dist" in tsconfig
    Prevents lots of logging during astro:check command

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,26 @@
-import type { Site, SocialObjects } from "./types";
+import type { Site } from "./types";
 
 export const SITE: Site = {
-  website: "https://joshkalsi.github.io", // replace this with your deployed domain
-  author: "Joshua Kalsi",
-  profile: "https://joshkalsi.dev/",
-  desc: "in the grim darkness of the far future there is only war.",
-  title: "Warzone Navio",
-  ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 3,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+    website: "https://joshkalsi.github.io", // replace this with your deployed domain
+    author: "Joshua Kalsi",
+    profile: "https://joshkalsi.dev/",
+    desc: "in the grim darkness of the far future there is only war.",
+    title: "Warzone Navio",
+    ogImage: "astropaper-og.jpg",
+    lightAndDarkMode: true,
+    postPerIndex: 4,
+    postPerPage: 3,
+    scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+    lang: "en", // html lang code. Set this empty and default will be "en"
+    langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
+    enable: false,
+    svg: true,
+    width: 216,
+    height: 46,
 };
