cc37244b25d204ae557b54ebc5b2b7b0b5d41c2c
Josh Kalsi <josh.kalsi@reachstudios.co.uk>
2024-09-01 14:43:14 +0100

    Add decap and remove old images

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import DecapCMS from 'astro-decap-cms';
 import { SITE } from "./src/config";
 
 // https://astro.build/config
@@ -15,6 +16,80 @@ export default defineConfig({
         }),
         react(),
         sitemap(),
+        DecapCMS({
+            config: {
+                backend: {
+                    name: "git-gateway",
+                    branch: "main"
+                },
+                media_folder: "src/assets/images",
+                public_folder: "src/assets/images",
+                collections: [
+                    {
+                        name: "battle",
+                        label: "Battle",
+                        folder: "src/content/battles",
+                        create: true,
+                        fields: [
+                            {
+                                label: "Author",
+                                name: "author",
+                                widget: "string",
+                                default: "Josh Kalsi"
+                            },
+                            {
+                                label: "Title",
+                                name: "title",
+                                widget: "string"
+                            },
+                            {
+                                label: "Slug",
+                                name: "slug",
+                                widget: "string"
+                            },
+                            {
+                                label: "Featured",
+                                name: "featured",
+                                widget: "boolean"
+                            },
+                            {
+                                label: "Draft",
+                                name: "draft",
+                                widget: "boolean"
+                            },
+                            {
+                                label: "Publish Date",
+                                name: "pubDateTime",
+                                widget: "datetime"
+                            },
+                            {
+                                label: "Modified Date",
+                                name: "modDateTime",
+                                widget: "datetime"
+                            },
+                            {
+                                label: "tags",
+                                name: "tags",
+                                widget: "list"
+                            },
+                            {
+                                label: "Description",
+                                name: "description",
+                                widget: "text"
+                            },
+                            {
+                                label: "Body",
+                                name: "body",
+                                widget: "markdown"
+                            }
+                        ]
+                    }
+                ]
+            },
+            previewStyles: [
+                '/src/styles/base.css',
+            ]
+        }),
     ],
     markdown: {
         remarkPlugins: [
